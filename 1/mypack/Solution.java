a16c3bf3963f5b2d6384723b12c640cc49d09b18
Alina Strusevich <strusevich.a.d@gmail.com>
2020-12-28 21:21:38 +0700

    3. Код переписан из функционального вида в ооп

---
+++ 1/mypack/Solution.java
@@ -0,0 +1,30 @@
+package mypack;
+
+import java.util.*;
+
+public class Solution {
+	/*
+	 * Дано n - число уровней. Построить треугольник из символов #. Пример для n =
+	 * 4: 
+	 *    # 
+	 *   ## 
+	 *  ### 
+	 * #### 
+	 * ----------------------------------------
+	 */
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		System.out.print("Enter n: ");
+		int n = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		StairsBuilder stairs = new StairsBuilder();
+		stairs.output(n);	
+	}
+
+}
+
+
---
+++ 1/mypack/StairsBuilder.java
@@ -0,0 +1,15 @@
+package mypack;
+
+public class StairsBuilder {
+    void output(int n) {
+		for (int stroke = 1; stroke <= n; stroke++) {
+			for (int symbol = 1; symbol <= n - stroke; symbol++) {
+				System.out.print(' ');
+			}
+			for (int symbol = 1; symbol <= stroke; symbol++) {
+				System.out.print('#');
+			}
+			System.out.print('\n');
+		}
+	}
+}
