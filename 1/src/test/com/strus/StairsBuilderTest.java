f203381dc75f80d66938be51552456cdbcee391c
Alina Strusevich <strusevich.a.d@gmail.com>
2021-01-21 13:41:32 +0700

    maven

---
+++ 1/src/test/com/strus/StairsBuilderTest.java
@@ -0,0 +1,17 @@
+package com.strus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StairsBuilderTest {
+
+    @Test
+    void output() {
+        assertEquals(false , false);
+    }
+
+    @Test
+    void getStairs() {
+    }
+}
